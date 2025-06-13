e179d3bb052cdda5e4030af383751cd989d58f71
Documenter.jl <documenter@juliadocs.github.io>
2025-06-13 14:32:57 +0000

    build based on db496ef

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#SuiteSplinesExamplePkg","page":"Home","title":"SuiteSplinesExamplePkg","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SuiteSplinesExamplePkg.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+[{"location":"#SuiteSplinesExamplePkg","page":"Home","title":"SuiteSplinesExamplePkg","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SuiteSplinesExamplePkg.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"#SuiteSplinesExamplePkg.testme-Tuple{Any}","page":"Home","title":"SuiteSplinesExamplePkg.testme","text":"testme(x)\n\nSome function to test.\n\n\n\n\n\n","category":"method"}]
 }
