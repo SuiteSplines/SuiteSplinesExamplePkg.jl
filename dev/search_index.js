327547e180edc217ba89e4bd8977d7d3bfe144c6
Documenter.jl <documenter@juliadocs.github.io>
2025-06-13 13:48:11 +0000

    build based on 3bf7d19

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#SuiteSplinesExamplePkg","page":"Home","title":"SuiteSplinesExamplePkg","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SuiteSplinesExamplePkg.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
